da8c81ca971c3f8e3dd40b7215f8cd7d8c80565a
David Pick <pickdavid@gmail.com>
2017-06-05 21:54:29 -0500

    Removing extra config key

--- test/configs/postgres_to_postgres.exs
+++ test/configs/postgres_to_postgres.exs
@@ -4,7 +4,6 @@ config(
   :etl,
   source_adapter: Etl.PostgresSource,
   destination_adapter: Etl.PostgresSink,
-  destination: "test/output",
   source_schema: "public",
   destination_schema: "test",
   source_connection: [
